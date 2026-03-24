789476319e4be5a5fc7e48f342d81686d2fa2602
Arrowmarkstudios <carlosgarciac0502@gmail.com>
2026-03-24 11:38:12 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+﻿// Smooth scroll
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('#contact').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Click interaction
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        alert("Book your appointment today 💈");
+    });
+});
